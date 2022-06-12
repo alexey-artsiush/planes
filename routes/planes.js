bb9306e06f7af97d76a0208d162357fa8eebf2da
Alexey <artush.alexey@gmail.com>
2022-06-12 22:51:07 +0300

    My first commit

---
+++ routes/planes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router()
+const { getPlanes, createPlane, getPlane } = require('../controllers/planes')
+const path = require('path')
+const multer = require('multer')
+
+//Показываем, где хранить загруженные файлы
+
+const storage = multer.diskStorage({
+    destination: './assets/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+
+    }
+})
+
+const upload = multer({ storage })
+//route GET/api/planes
+//@des получить все самолеты
+router.get('/', getPlanes)
+
+//route GET/api/planes/:id
+//@des получить самолет по id
+router.get('/:id', getPlane)
+
+//route GET/api/planes
+//@des Создать самолет
+router.post('/', upload.single('planeImage'), createPlane)
+
+module.exports = router
